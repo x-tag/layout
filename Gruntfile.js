7d8a730d9a105842388f88f4e241dda043d777a5
Daniel Buchner <danieljb2@gmail.com>
2013-05-20 16:13:18 -0700

    first push of view element

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint:{
+      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    'smush-components': {
+      options: {
+        fileMap: {
+          js: 'demo/x-tag-components.js',
+          css: 'demo/x-tag-components.css'
+        }
+      }
+    },
+    bumpup: ['component.json', 'package.json', 'xtag.json'],
+    tagrelease: {
+      file: 'package.json',
+      prefix: 'xtag-v',
+      commit: true
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bumpup');
+  grunt.loadNpmTasks('grunt-tagrelease');
+  grunt.loadNpmTasks('grunt-smush-components');
+
+  grunt.registerTask('build', ['jshint','smush-components']);
+  grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
+
+};
