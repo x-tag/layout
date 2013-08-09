aed82a44dd09126110bcbaec810a1d65581b3703
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-08-09 16:14:36 -0700

    update demo page to work with brick site demo helpers

--- demo/demo.js
+++ demo/demo.js
@@ -1,27 +1,16 @@
 function updateDemoSect(demoSect){
-    var markupEl = getMarkupEl(demoSect, "html");
-    var contextEl = getContextEl(demoSect);
+    var markupEl = DemoHelpers.getMarkupEl(demoSect, "html");
+    var contextEl = DemoHelpers.getContextEl(demoSect);
 
-    var htmlMarkup = cleanHtmlSource(contextEl.innerHTML, ["style", "content-maximizing"]);
-    updatePrettyprintEl(markupEl, htmlMarkup);
+    var htmlMarkup = DemoHelpers.cleanHtmlSource(contextEl.innerHTML, 
+                                               ["style", "content-maximizing"]);
+    DemoHelpers.updatePrettyprintEl(markupEl, htmlMarkup);
 }
 
 document.addEventListener('DOMComponentsLoaded', function(){
-    xtag.addEvent(document, "click:delegate(.demo-wrap > .markup-wrap > button.toggle)", function(e){
-        var button = this;
-        var demoSect = button.parentNode.parentNode;
-        var toggleAttr = this.getAttribute("data-toggle-attr");
-        if(!toggleAttr) return;
-
-        var layoutEl = demoSect.querySelector("x-layout");
-        layoutEl[toggleAttr] = !layoutEl[toggleAttr];
-
-        updateDemoSect(demoSect);
+    xtag.addEvent(document, "update-demo:delegate("+DemoHelpers.DEMO_SECT_SELECTOR+")", function(e){
+        updateDemoSect(this);
     });
 
-    xtag.query(document, ".demo-wrap").forEach(function(demoSect){
-        updateDemoSect(demoSect);
-    });
-
-    prettyPrint();
+    DemoHelpers.initializeDemos();
 });
