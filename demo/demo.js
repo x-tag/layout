f9bb10dddc926f98da7b82b2376f1852527ecb9f
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-08-09 11:35:28 -0700

    add maxcontent demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,27 @@
+function updateDemoSect(demoSect){
+    var markupEl = getMarkupEl(demoSect, "html");
+    var contextEl = getContextEl(demoSect);
+
+    var htmlMarkup = cleanHtmlSource(contextEl.innerHTML, ["style", "content-maximizing"]);
+    updatePrettyprintEl(markupEl, htmlMarkup);
+}
+
 document.addEventListener('DOMComponentsLoaded', function(){
+    xtag.addEvent(document, "click:delegate(.demo-wrap > .markup-wrap > button.toggle)", function(e){
+        var button = this;
+        var demoSect = button.parentNode.parentNode;
+        var toggleAttr = this.getAttribute("data-toggle-attr");
+        if(!toggleAttr) return;
+
+        var layoutEl = demoSect.querySelector("x-layout");
+        layoutEl[toggleAttr] = !layoutEl[toggleAttr];
+
+        updateDemoSect(demoSect);
+    });
+
+    xtag.query(document, ".demo-wrap").forEach(function(demoSect){
+        updateDemoSect(demoSect);
+    });
+
     prettyPrint();
 });
